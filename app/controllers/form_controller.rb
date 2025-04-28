3aae82c395b5215f0671545ae221777e4ed4c19d
erose3 <eli.rosenthal3@gmail.com>
2025-04-28 19:11:31 +0000

    form+result

--- app/controllers/form_controller.rb
+++ app/controllers/form_controller.rb
@@ -3,4 +3,11 @@ class FormController < ApplicationController
     render ({ :template => "form_templates/blank"})
   end
 
+  def result
+    @image = params.fetch("image_param")
+    @description = params.fetch("description_param")
+
+    render ({ :template => "form_templates/result"})
+  end
+
 end
